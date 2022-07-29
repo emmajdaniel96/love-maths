9c816c9a1dcabb82cd21d8a69f7602bac24ad694
emmajdaniel96 <emmajdaniel96@googlemail.com>
2022-07-29 07:53:28 +0000

    set up structure of gane

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkGame() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
